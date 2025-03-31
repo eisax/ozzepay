33f75907cf63d0da6624b6fd74da8a169a8c54ce
Josphat Kuda Ndhlovu <josphatndhlovu@Josphats-MacBook-Pro.local>
2025-03-31 15:15:47 +0200

    feat:update 1

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,20 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { TooltipProvider } from "@/components/ui/tooltip";
+import { AuthProvider } from './context/AuthContext';
+import { Toaster } from './components/ui/toaster';
 
 const queryClient = new QueryClient();
 
-const App=()=>(
-  <QueryClientProvider client={queryClient}></QueryClientProvider>
+const App = () => (
+  <QueryClientProvider client={queryClient}>
+    <TooltipProvider>
+      <AuthProvider>
+        <Toaster />
+      </AuthProvider>
+    </TooltipProvider>
+  </QueryClientProvider>
 );
 
 export default App
