f4a4411978d3ff1a3c0a8b69511f72ab345de4e7
Kudah Ndhlovu <eisaxoffice@gmail.com>
2025-04-10 18:48:44 +0200

    feat:update commit, please review  commit 2025-04-10 18:48:44

--- src/pages/PayBills.tsx
+++ src/pages/PayBills.tsx
@@ -71,6 +71,14 @@ const PayBills = () => {
                                 </Select>
                             </div>
                         </div>
+
+                        <div className="space-y-2">
+              <Label htmlFor="amount">Amount (USD)</Label>
+              <div className="relative">
+                <span className="absolute left-3 top-1/2 -translate-y-1/2 text-muted-foreground">$</span>
+                <Input id="amount" type="number" className="pl-7" placeholder="0.00" />
+              </div>
+            </div>
                     </form>
                 </CardContent>
             </Card>
