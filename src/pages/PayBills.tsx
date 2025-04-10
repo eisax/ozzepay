b5d7fe641daa124483c84bb72f53758a419387a5
Kudah Ndhlovu <eisaxoffice@gmail.com>
2025-04-10 17:37:58 +0200

    feat:update commit, please review  commit 2025-04-10 17:37:58

--- src/pages/PayBills.tsx
+++ src/pages/PayBills.tsx
@@ -12,7 +12,9 @@ const PayBills = () => {
     };
 
     return (
-        <div className="space-y-6"></div>
+        <div className="space-y-6">
+            <h1 className="text-2xl font-bold">Pay Bills</h1>
+        </div>
     );
 }
 export default PayBills;
