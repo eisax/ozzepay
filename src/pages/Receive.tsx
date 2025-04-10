555a5577dad1c33cdefcc942e6e8fb68e2db3895
Kudah Ndhlovu <eisaxoffice@gmail.com>
2025-04-10 16:28:32 +0200

    feat:update commit, please review  commit 2025-04-10 16:28:32

--- src/pages/Receive.tsx
+++ src/pages/Receive.tsx
@@ -12,7 +12,9 @@ const Receive = () => {
     };
 
     return (
-        <div className="space-y-6"></div>
+        <div className="space-y-6">
+            <h1 className="text-2xl font-bold">Receive Money</h1>
+        </div>
     );
 }
 
