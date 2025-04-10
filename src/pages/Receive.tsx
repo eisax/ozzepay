5c60aa6ac99138c66364cd2dd47b3d46d5930547
Kudah Ndhlovu <eisaxoffice@gmail.com>
2025-04-10 16:26:42 +0200

    feat:update commit, please review  commit 2025-04-10 16:26:42

--- src/pages/Receive.tsx
+++ src/pages/Receive.tsx
@@ -2,6 +2,14 @@ import { useToast } from "@/hooks/use-toast";
 
 const Receive = () => {
     const { toast } = useToast();
+
+    const copyToClipboard = () => {
+        navigator.clipboard.writeText("wallet-id-12345");
+        toast({
+          title: "Wallet ID copied!",
+          description: "You can now share this with the sender",
+        });
+      };
 }
 
 export default Receive;
