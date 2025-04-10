96ac193b65e144f85ce6b9bbd8a7bf1f5921bb30
Kudah Ndhlovu <eisaxoffice@gmail.com>
2025-04-10 15:12:35 +0200

    feat:update commit, please review  commit 2025-04-10 15:12:34

--- src/components/modals/SendMoneyModal.tsx
+++ src/components/modals/SendMoneyModal.tsx
@@ -7,7 +7,7 @@ import {
   DialogFooter,
   DialogHeader,
   DialogTitle,
-} from "@/components/ui/dialod";
+} from "@/components/ui/dialog";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
