5cfd3f2208f10900007528e1b749ba8a937d9e4a
Kudah Ndhlovu <eisaxoffice@gmail.com>
2025-04-10 16:05:41 +0200

    feat:update commit, please review  commit 2025-04-10 16:05:41

--- src/App.tsx
+++ src/App.tsx
@@ -14,6 +14,7 @@ import './App.css';
 import { AppLayout } from "./components/layout/AppLayout";
 import Index from "./pages/Index";
 import Transactions from "./pages/Transactions";
+import Send from "./pages/Send";
 
 
 
