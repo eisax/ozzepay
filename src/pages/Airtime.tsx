19444b3ccb730fc56132f5162dd7c6789e013728
Kudah Ndhlovu <eisaxoffice@gmail.com>
2025-04-10 20:58:24 +0200

    feat:update commit, please review  commit 2025-04-10 20:58:24

--- src/pages/Airtime.tsx
+++ src/pages/Airtime.tsx
@@ -1,15 +1,17 @@
 import { useToast } from "@/hooks/use-toast";
 
-const Airtime = () => { 
+const Airtime = () => {
     const { toast } = useToast();
 
     const handlePurchase = (e: React.FormEvent) => {
         e.preventDefault();
         toast({
-          title: "Airtime purchased",
-          description: "Your airtime has been successfully purchased",
+            title: "Airtime purchased",
+            description: "Your airtime has been successfully purchased",
         });
-      };
+    };
+
+    return ()
 }
 
 export default Airtime;
