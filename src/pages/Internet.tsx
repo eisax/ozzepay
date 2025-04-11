5a3cd39056b77b3b8e9e5e3843f2462d7fd3bf92
Kudah Ndhlovu <eisaxoffice@gmail.com>
2025-04-11 17:45:40 +0200

    feat:update commit, please review  commit 2025-04-11 17:45:40

--- src/pages/Internet.tsx
+++ src/pages/Internet.tsx
@@ -1,6 +1,8 @@
 import { useToast } from "@/hooks/use-toast";
 import { Wifi } from "lucide-react";
 import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
 
 const Internet = () => {
