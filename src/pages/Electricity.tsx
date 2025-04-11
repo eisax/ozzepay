1c5d940d4779461c8733bc1221fb4692d3a9a632
Kudah Ndhlovu <eisaxoffice@gmail.com>
2025-04-11 17:52:29 +0200

    feat:update commit, please review  commit 2025-04-11 17:52:29

--- src/pages/Electricity.tsx
+++ src/pages/Electricity.tsx
@@ -13,7 +13,9 @@ const Electricity = () => {
 
 
     return (
-        <div className="space-y-6"></div>
+        <div className="space-y-6">
+            <h1 className="text-2xl font-bold">Buy Electricity</h1>
+        </div>
     );
 };
 
