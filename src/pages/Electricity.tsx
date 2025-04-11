305334bef4e094701b9d113b06e0a233c527ccff
Kudah Ndhlovu <eisaxoffice@gmail.com>
2025-04-11 17:52:59 +0200

    feat:update commit, please review  commit 2025-04-11 17:52:59

--- src/pages/Electricity.tsx
+++ src/pages/Electricity.tsx
@@ -1,4 +1,5 @@
 import { useToast } from "@/hooks/use-toast";
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
 
 const Electricity = () => {
     const { toast } = useToast();
