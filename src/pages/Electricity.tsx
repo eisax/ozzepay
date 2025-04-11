993221f232cffe9948e4e85ebacc869d1599b2e1
Kudah Ndhlovu <eisaxoffice@gmail.com>
2025-04-11 17:51:47 +0200

    feat:update commit, please review  commit 2025-04-11 17:51:47

--- src/pages/Electricity.tsx
+++ src/pages/Electricity.tsx
@@ -2,6 +2,14 @@ import { useToast } from "@/hooks/use-toast";
 
 const Electricity = () => {
     const { toast } = useToast();
- };
+
+    const handlePurchase = (e: React.FormEvent) => {
+        e.preventDefault();
+        toast({
+            title: "Electricity purchased",
+            description: "Your electricity token has been generated",
+        });
+    };
+};
 
 export default Electricity;
