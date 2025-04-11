5c756ac139de1c8f28c4feecbc3107a4365d250c
Kudah Ndhlovu <eisaxoffice@gmail.com>
2025-04-11 17:43:11 +0200

    feat:update commit, please review  commit 2025-04-11 17:43:11

--- src/pages/Internet.tsx
+++ src/pages/Internet.tsx
@@ -1,4 +1,5 @@
 import { useToast } from "@/hooks/use-toast";
+import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
 
 const Internet = () => {
