57a3b3bd5f0508a6682a653dc9d62aea50758367
Kudah Ndhlovu <eisaxoffice@gmail.com>
2025-04-11 18:41:56 +0200

    feat:update commit, please review  commit 2025-04-11 18:41:56

--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -12,7 +12,9 @@ const Profile = () => {
             <h1 className="text-2xl font-bold">Profile</h1>
 
             <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
-            <Card className="md:col-span-1"></Card>
+            <Card className="md:col-span-1">
+            <CardHeader className="pb-2"></CardHeader>
+            </Card>
             </div>
         </div>
     );
