719016e3374cbf72efac77c3799766b3b65e48cc
Kudah Ndhlovu <eisaxoffice@gmail.com>
2025-04-11 17:45:21 +0200

    feat:update commit, please review  commit 2025-04-11 17:45:21

--- src/pages/Internet.tsx
+++ src/pages/Internet.tsx
@@ -43,7 +43,9 @@ const Internet = () => {
                     </div>
 
                     <form onSubmit={handlePay} className="space-y-4">
-                        <div className="space-y-2"></div>
+                        <div className="space-y-2">
+                            <Label htmlFor="account">Account Number</Label>
+                        </div>
                     </form>
                 </CardContent>
             </Card>
