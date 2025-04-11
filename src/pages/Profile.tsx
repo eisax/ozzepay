5ed5ffbcc06c8bc3476b12d2dac9e82269c23a32
Kudah Ndhlovu <eisaxoffice@gmail.com>
2025-04-11 19:22:26 +0200

    feat:update commit, please review  commit 2025-04-11 19:22:26

--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -69,10 +69,10 @@ const Profile = () => {
                     <CardContent className="pt-6">
                         <Tabs defaultValue="personal">
                             <TabsList className="mb-6">
-                            <TabsTrigger value="personal">
-                  <User size={16} className="mr-2" />
-                  Personal
-                </TabsTrigger>
+                                <TabsTrigger value="personal">
+                                    <User size={16} className="mr-2" />
+                                    Personal
+                                </TabsTrigger>
                             </TabsList>
                         </Tabs>
                     </CardContent>
