f0fdb322f46cb255851270563e53881184948ae9
Kudah Ndhlovu <eisaxoffice@gmail.com>
2025-04-11 18:42:48 +0200

    feat:update commit, please review  commit 2025-04-11 18:42:48

--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -5,6 +5,7 @@ import {
     CardHeader,
     CardTitle,
 } from "@/components/ui/card";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 
 const Profile = () => {
     return (
