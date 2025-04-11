c309e17dcc45c48dc1cdf47da4dcabf6966bbe98
Kudah Ndhlovu <eisaxoffice@gmail.com>
2025-04-11 17:39:11 +0200

    feat:update commit, please review  commit 2025-04-11 17:39:11

--- src/pages/Internet.tsx
+++ src/pages/Internet.tsx
@@ -2,6 +2,14 @@ import { useToast } from "@/hooks/use-toast";
 
 const Internet = () => {
     const { toast } = useToast();
- }
+
+    const handlePay = (e: React.FormEvent) => {
+        e.preventDefault();
+        toast({
+            title: "Internet package purchased",
+            description: "Your internet package has been successfully purchased",
+        });
+    };
+}
 
 export default Internet;
