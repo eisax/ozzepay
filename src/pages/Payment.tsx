0ffc1153238e8ba50e3f210990bae7368104d36e
Kudah Ndhlovu <eisaxoffice@gmail.com>
2025-04-12 18:37:50 +0200

    feat:update commit, please review  commit 2025-04-12 18:37:50

--- src/pages/Payment.tsx
+++ src/pages/Payment.tsx
@@ -299,7 +299,9 @@ const Payment = () => {
                                         </Select>
                                     </div>
 
-                                    <div className="space-y-2"></div>
+                                    <div className="space-y-2">
+                                        <Label htmlFor="mobile-amount">Amount (USD)</Label>
+                                    </div>
                                 </div>
                             </form>
                         </TabsContent>
