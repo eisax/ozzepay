f5fe6a55ab1a83f7a4f3abb747e7ff65d7c7a1d6
Kudah Ndhlovu <eisaxoffice@gmail.com>
2025-04-12 22:33:15 +0200

    feat:update commit, please review  commit 2025-04-12 22:33:15

--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -424,6 +424,8 @@ const Profile = () => {
                                             </div>
                                         </div>
                                     </div>
+
+                                    <div className="space-y-4 pt-4 border-t"></div>
                                 </div>
                             </TabsContent>
                         </Tabs>
