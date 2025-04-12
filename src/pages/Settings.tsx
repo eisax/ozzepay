8cdb16c314020f42bf1ee843053b45428288d934
Kudah Ndhlovu <eisaxoffice@gmail.com>
2025-04-12 21:50:02 +0200

    feat:update commit, please review  commit 2025-04-12 21:50:02

--- src/pages/Settings.tsx
+++ src/pages/Settings.tsx
@@ -376,6 +376,8 @@ const Settings = () => {
                                 </div>
                                 <Switch defaultChecked />
                             </div>
+
+                            <div className="flex items-center justify-between"></div>
                         </CardContent>
                     </Card>
                 </TabsContent>
