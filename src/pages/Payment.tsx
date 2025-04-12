44c43b9e7875148d954bb2cc2b06468dd7c637df
Kudah Ndhlovu <eisaxoffice@gmail.com>
2025-04-12 18:32:45 +0200

    feat:update commit, please review  commit 2025-04-12 18:32:45

--- src/pages/Payment.tsx
+++ src/pages/Payment.tsx
@@ -241,6 +241,10 @@ const Payment = () => {
                                     <Smartphone className="h-6 w-6 text-blue-500" />
                                     <span>Telecel</span>
                                 </Button>
+
+                                <Button variant="outline" className="h-20 flex flex-col items-center justify-center gap-2">
+                                    
+                                </Button>
                             </div>
                         </TabsContent>
                     </Tabs>
