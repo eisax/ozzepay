c4f205cd3cc96283f169ddbd9c32a1a69ac04110
Kudah Ndhlovu <eisaxoffice@gmail.com>
2025-04-12 22:19:27 +0200

    feat:update commit, please review  commit 2025-04-12 22:19:27

--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -5,7 +5,7 @@ import {
     CardHeader,
     CardTitle,
 } from "@/components/ui/card";
-
+import { Switch } from "@/components/ui/switch";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import {
