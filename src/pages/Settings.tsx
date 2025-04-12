5409e13d1fe0e504ed0f1bb40274a10e102108de
Kudah Ndhlovu <eisaxoffice@gmail.com>
2025-04-12 20:56:12 +0200

    feat:update commit, please review  commit 2025-04-12 20:56:12

--- src/pages/Settings.tsx
+++ src/pages/Settings.tsx
@@ -53,7 +53,9 @@ const Settings = () => {
                         </CardHeader>
 
                         <CardContent className="space-y-4">
-                            <div className="grid grid-cols-1 md:grid-cols-2 gap-4"></div>
+                            <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+                                <div className="space-y-2"></div>
+                            </div>
                         </CardContent>
                     </Card>
                 </TabsContent>
