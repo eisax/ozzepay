312fd0139b9f520edad4e3059964b5e407268d04
Kudah Ndhlovu <eisaxoffice@gmail.com>
2025-04-12 18:32:07 +0200

    feat:update commit, please review  commit 2025-04-12 18:32:07

--- src/pages/Payment.tsx
+++ src/pages/Payment.tsx
@@ -238,7 +238,7 @@ const Payment = () => {
                                 </Button>
 
                                 <Button variant="outline" className="h-20 flex flex-col items-center justify-center gap-2">
-                                <Smartphone className="h-6 w-6 text-blue-500" />
+                                    <Smartphone className="h-6 w-6 text-blue-500" />
                                 </Button>
                             </div>
                         </TabsContent>
