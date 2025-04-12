c616df297bf2c1805bde8f3b875a9af592a39186
Kudah Ndhlovu <eisaxoffice@gmail.com>
2025-04-12 21:49:35 +0200

    feat:update commit, please review  commit 2025-04-12 21:49:35

--- src/pages/Settings.tsx
+++ src/pages/Settings.tsx
@@ -368,7 +368,9 @@ const Settings = () => {
                             <CardDescription>Choose what notifications you receive</CardDescription>
                         </CardHeader>
 
-                        <CardContent className="space-y-4"></CardContent>
+                        <CardContent className="space-y-4">
+                        <div className="flex items-center justify-between"></div>
+                        </CardContent>
                     </Card>
                 </TabsContent>
             </Tabs>
