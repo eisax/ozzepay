9b1c90af3b7c603aa35314c9f811b5a9c46a0422
Kudah Ndhlovu <eisaxoffice@gmail.com>
2025-04-12 22:12:58 +0200

    feat:update commit, please review  commit 2025-04-12 22:12:58

--- src/pages/Request.tsx
+++ src/pages/Request.tsx
@@ -4,6 +4,7 @@ import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/com
 import { Receipt, Copy, Check } from "lucide-react";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
+import { Textarea } from "@/components/ui/textarea";
 
 const Request = () => {
 
