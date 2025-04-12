c9c4c23533a692ad45cf74b1eb619816067a023a
Kudah Ndhlovu <eisaxoffice@gmail.com>
2025-04-12 22:07:19 +0200

    feat:update commit, please review  commit 2025-04-12 22:07:19

--- src/pages/Request.tsx
+++ src/pages/Request.tsx
@@ -14,7 +14,7 @@ const Request = () => {
     };
 
     const copyPaymentLink = () => {
-        navigator.clipboard.writeText("https://ozzepay.app/pay/req123456");
+        navigator.clipboard.writeText("https://ozzepay.ozzene/pay/req123456");
         setLinkCopied(true);
         toast({
             title: "Link copied!",
