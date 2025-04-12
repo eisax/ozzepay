49cee1e31ee1901b73fcfce5d8fd8ef86fa51fcb
Kudah Ndhlovu <eisaxoffice@gmail.com>
2025-04-12 18:54:52 +0200

    feat:update commit, please review  commit 2025-04-12 18:54:52

--- src/pages/Payment.tsx
+++ src/pages/Payment.tsx
@@ -371,6 +371,8 @@ const Payment = () => {
                                         </Select>
                                     </div>
                                 </div>
+
+                                <div className="grid grid-cols-1 md:grid-cols-2 gap-4"></div>
                             </form>
                         </TabsContent>
                     </Tabs>
