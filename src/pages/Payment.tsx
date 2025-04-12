47198352bb05bc4491071e93ea13a857f7b716ee
Kudah Ndhlovu <eisaxoffice@gmail.com>
2025-04-12 18:51:18 +0200

    feat:update commit, please review  commit 2025-04-12 18:51:18

--- src/pages/Payment.tsx
+++ src/pages/Payment.tsx
@@ -341,6 +341,11 @@ const Payment = () => {
                                     <Wifi className="h-6 w-6 text-orange-500" />
                                     <span>Liquid Home</span>
                                 </Button>
+
+                                <Button variant="outline" className="h-24 flex flex-col items-center justify-center gap-2">
+                                    <Wifi className="h-6 w-6 text-orange-500" />
+                                    <span></span>
+                                </Button>
                             </div>
                         </TabsContent>
                     </Tabs>
