4a9ae297d1b821b32e4f2da7203199bd5a9ba817
Kudah Ndhlovu <eisaxoffice@gmail.com>
2025-04-12 21:49:10 +0200

    feat:update commit, please review  commit 2025-04-12 21:49:10

--- src/pages/Settings.tsx
+++ src/pages/Settings.tsx
@@ -361,7 +361,9 @@ const Settings = () => {
                     </Card>
                 </TabsContent>
 
-                <TabsContent value="notifications" className="mt-6 space-y-6"></TabsContent>
+                <TabsContent value="notifications" className="mt-6 space-y-6">
+                <Card></Card>
+                </TabsContent>
             </Tabs>
         </div>
     );
