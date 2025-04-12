5044bfdb26f81e53641b3cb87fff1f400def8398
Kudah Ndhlovu <eisaxoffice@gmail.com>
2025-04-12 20:32:41 +0200

    feat:update commit, please review  commit 2025-04-12 20:32:41

--- src/pages/Analytics.tsx
+++ src/pages/Analytics.tsx
@@ -170,7 +170,7 @@ const Analytics = () => {
             </div>
 
             <div className="grid grid-cols-1 lg:grid-cols-2 gap-6">
-            <Card className="col-span-1 lg:col-span-2">
+                <Card className="col-span-1 lg:col-span-2"></Card>
             </div>
         </div>
     );
